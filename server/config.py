766b4c59f4e68d45dd6547ac6aefb13c1fe74afb
Taylor Ng <taylormng95@gmail.com>
2024-08-06 17:21:16 -0400

    configures SQLAlchemy

--- server/config.py
+++ server/config.py
@@ -1,3 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.schema import MetaData
+from flask_migrate import Migrate
+
+metadata = MetaData(naming_convention={
+        "ix": 'ix_%(column_0_label)s',
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    })
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db' # change to OS and env file
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app=app, metadata=metadata)
+
+migrate = Migrate(app=app, db=db, render_as_batch=True)
---
+++ server/migrations/versions/8383dc70cbb4_init_database.py
@@ -0,0 +1,24 @@
+"""init database
+
+Revision ID: 8383dc70cbb4
+Revises: 
+Create Date: 2024-08-06 17:15:51.731705
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8383dc70cbb4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
