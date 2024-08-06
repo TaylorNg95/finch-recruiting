1e4c167025065bca8916c511fac558fb64a9e9f4
Taylor Ng <taylormng95@gmail.com>
2024-08-06 17:59:54 -0400

    adds user pw hashing and backend validations

--- server/config.py
+++ server/config.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.schema import MetaData
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 
 metadata = MetaData(naming_convention={
         "ix": 'ix_%(column_0_label)s',
@@ -19,3 +20,5 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app=app, metadata=metadata)
 
 migrate = Migrate(app=app, db=db, render_as_batch=True)
+
+bcrypt = Bcrypt(app)
--- server/models/user.py
+++ server/models/user.py
@@ -1,6 +1,8 @@
-from config import db
+from config import db, bcrypt
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import validates
+import re
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
@@ -15,4 +17,33 @@ class User(db.Model, SerializerMixin):
     def __repr__(self):
         return f'<User id={self.id} name={self.name}>'
 
+    @hybrid_property
+    def password_hash(self):
+        if self._password_hash == '':
+            raise ValueError('Password required')
+        else:
+            raise AttributeError('Cannot be accessed')
+        
+    @password_hash.setter
+    def password_hash(self, password):
+        pw_hash = bcrypt.generate_password_hash(password)
+        self._password_hash = pw_hash.decode()
 
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
+    
+    @validates('name', 'username')
+    def check_inputs(self, key, input):
+        if input == '':
+            raise ValueError('Name and username required')
+        else:
+            return input
+        
+    @validates('email')
+    def check_email(self, key, email):
+        if email == '':
+            raise ValueError('Email required')
+        elif not re.fullmatch(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
+            raise ValueError('Email invalid')
+        else:
+            return email
