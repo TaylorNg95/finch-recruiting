51f73da02024578f97bcffbb82aa59187b066ef9
Taylor Ng <taylormng95@gmail.com>
2024-08-07 11:32:18 -0400

    adds Browser Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react"
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
-    <h1>Phase 5 Project</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='' element=''/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
