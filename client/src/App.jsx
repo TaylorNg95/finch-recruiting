1d78fd4edab407a3a9da7bebc5a35a3ba6f8a5dc
Taylor Ng <taylormng95@gmail.com>
2024-08-07 11:43:24 -0400

    adds routes and components for Home, Signup and Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import NavBar from "./components/navigation/NavBar"
+import Home from "./components/Home"
+import Signup from "./components/sessions/Signup"
+import Login from "./components/sessions/Login"
 
 function App() {
 
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
-        <Route path='' element=''/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/login' element={<Login />}/>
       </Routes>
     </BrowserRouter>
   )
