d62894b59299049f51f224f2e7ae64a02ff1f7eb
Taylor Ng <taylormng95@gmail.com>
2024-08-12 18:25:49 -0400

    refactors emails into html templates

--- server/app.py
+++ server/app.py
@@ -10,8 +10,8 @@ from routes.meetingTypes import *
 from jobs.mail import sendWeeklyEmail, sendTouchpointReminder
 
 if __name__ == '__main__':
-    scheduler.add_job(func=sendWeeklyEmail, trigger='cron', day_of_week=0, hour=16, minute=36, second=50, id='weeklyUpdateJob')
-    scheduler.add_job(func=sendTouchpointReminder, trigger='cron', hour=9, minute=0, second=0, id='contactReminderJob')
+    scheduler.add_job(func=sendWeeklyEmail, trigger='cron', day_of_week=0, hour=18, minute=23, second=5, id='weeklyUpdateJob')
+    scheduler.add_job(func=sendTouchpointReminder, trigger='cron', hour=18, minute=23, second=50, id='contactReminderJob')
     
     scheduler.start()
     app.run(port=5555, debug=False)
--- server/jobs/mail.py
+++ server/jobs/mail.py
@@ -1,44 +1,39 @@
 from config import app, mail
 import datetime as dt
 from flask_mail import Message
+from flask import render_template
 from models.user import User
 from models.recruit import Recruit
 from models.touchpoint import Touchpoint
-from jobs.helpers import weeklySummaryEmail, touchpointReminderEmail
 
 today = dt.datetime.now().date().isoformat() # current date
 week_ago = (dt.datetime.now() - dt.timedelta(days=7)).date().isoformat() # date one week ago
 
 def sendWeeklyEmail():
     with app.app_context():
-        users = User.query.all()
-        all_touchpoints = Touchpoint.query.all()
-        all_touchpoints_this_week = [touchpoint for touchpoint in all_touchpoints if week_ago <= touchpoint.date <= today]
-        for user in users:
+        all_tp_this_week = [tp for tp in Touchpoint.query.all() if week_ago <= tp.date <= today]
+        for user in User.query.all():
             user_recruit_ids = [recruit.id for recruit in user.recruits]
-            user_touchpoints_this_week = sorted([touchpoint for touchpoint in all_touchpoints_this_week if touchpoint.recruit_id in user_recruit_ids], key=lambda x:x.date, reverse=True)
-            # this works because recruits are unique to users, so you only need to compare the touchpoint_recruit_ids
-            message_strings = [f"• {touchpoint.date} || {touchpoint.meetingType.type} with {touchpoint.recruit.first_name} {touchpoint.recruit.last_name}<br>" for touchpoint in user_touchpoints_this_week] if user_touchpoints_this_week else 'No logged activity.'
-
-            msg = Message(
-                subject='Your Weekly Summary',
-                recipients=[user.email]
-            )
-            msg.html = weeklySummaryEmail(user.first_name, ''.join(message_strings))
+            user_tp_this_week = sorted([tp for tp in all_tp_this_week if tp.recruit_id in user_recruit_ids], key=lambda x:x.date, reverse=True)
+        
+            message_strings = [f"{tp.date} || {tp.meetingType.type} with {tp.recruit.first_name} {tp.recruit.last_name}" for tp in user_tp_this_week] if user_tp_this_week else ['No logged activity']
+            msg = generate_message(user, 'Your Weekly Summary', message_strings, 'weekly_summary.html')
             mail.send(msg)
 
 def sendTouchpointReminder():
     with app.app_context():
-        recruitsToContact = Recruit.query.filter(Recruit.next_touchpoint == today)
-        usersToContact = set([recruit.user for recruit in recruitsToContact])
-        if not usersToContact:
-            return None # don't send email if no users to contact
-        for user in usersToContact:
-            userRecruits = [recruit for recruit in recruitsToContact if recruit.user_id == user.id]
-            message_strings = [f"• {recruit.first_name} {recruit.last_name}<br>" for recruit in userRecruits]
-            msg = Message(
-                subject='Your Recruiting Reminders',
-                recipients=[user.email]
-            )
-            msg.html = touchpointReminderEmail(user.first_name, ''.join(message_strings))
+        recruits_to_contact = Recruit.query.filter(Recruit.next_touchpoint == today)
+        if not recruits_to_contact:
+            return None # don't send email if no recruits to contact
+        users_to_contact = set([recruit.user for recruit in recruits_to_contact])
+        for user in users_to_contact:
+            user_recruits = [recruit for recruit in recruits_to_contact if recruit.user_id == user.id]
+            message_strings = [f"{recruit.first_name} {recruit.last_name}" for recruit in user_recruits]
+            msg = generate_message(user, 'Your Recruiting Reminders', message_strings, 'touchpoint_reminder.html')
             mail.send(msg)
+
+def generate_message(user, subject, items, template):
+    msg = Message(subject=subject, recipients=[user.email])
+    template_path = f'emails/{template}'
+    msg.html = render_template(template_path, name=user.first_name, items=items)
+    return msg
