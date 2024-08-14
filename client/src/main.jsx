e38b7338e44080cc99bbdf7d30b012b052f4c0ac
Taylor Ng <taylormng95@gmail.com>
2024-08-14 15:24:55 -0400

    adds theme to project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ThemeProvider } from '@emotion/react'
+import theme from './theme'
+import { CssBaseline } from '@mui/material'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+    </ThemeProvider>
 )
