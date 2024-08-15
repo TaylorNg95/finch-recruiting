4994d45f87deabfa5c350a4b35d08ddada536ebe
Taylor Ng <taylormng95@gmail.com>
2024-08-15 13:34:10 -0400

    updates for render deployment

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-from config import app, scheduler
+from flask import render_template
+from config import app
 from models.user import User
 from models.recruit import Recruit
 from models.meetingType import MeetingType
@@ -8,6 +9,10 @@ from routes.recruits import *
 from routes.touchpoints import *
 from routes.meetingTypes import *
 from routes.emails import *
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
