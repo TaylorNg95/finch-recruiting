9ef02b445a26eb0122b9f2a49b1e57a757ced76f
Taylor Ng <taylormng95@gmail.com>
2024-08-15 11:59:04 -0400

    reads html files rather than rendering templates

--- server/emails/email.py
+++ server/emails/email.py
@@ -15,19 +15,23 @@ def sendWeeklySummary(user):
     user_recruit_ids = [recruit.id for recruit in user.recruits]
     all_tp_this_week = [tp for tp in Touchpoint.query.all() if week_ago <= tp.date <= today]
     user_tp_this_week = sorted([tp for tp in all_tp_this_week if tp.recruit_id in user_recruit_ids], key=lambda x:x.date, reverse=True)
-
-    message_strings = [f"{tp.date[5:] + '-' + tp.date[2:4]}: {tp.meetingType.type} with {tp.recruit.first_name} {tp.recruit.last_name}" for tp in user_tp_this_week] if user_tp_this_week else ['No logged activity']
+    message_strings = ''.join([f"<li>{tp.date[5:] + '-' + tp.date[2:4]}: {tp.meetingType.type} with {tp.recruit.first_name} {tp.recruit.last_name}</li>" for tp in user_tp_this_week]) if user_tp_this_week else '<li>No logged activity</li>'
     msg = generate_message(user, 'Your Weekly Activity Summary', 'weekly_summary.html', message_strings)
     mail.send(msg)
 
 def sendTouchpointReminder(user):
     recruits_to_contact = [recruit for recruit in user.recruits if recruit.next_touchpoint == today]
-    message_strings = [f"{recruit.first_name} {recruit.last_name}" for recruit in recruits_to_contact] if recruits_to_contact else ['No outstanding reminders']
+    message_strings = ''.join([f"<li>{recruit.first_name} {recruit.last_name}</li>" for recruit in recruits_to_contact]) if recruits_to_contact else '<li>No outstanding reminders</li>'
     msg = generate_message(user, 'Your Contact Reminders', 'touchpoint_reminder.html', message_strings)
     mail.send(msg)
 
 def generate_message(user, subject, template, items=None):
     msg = Message(subject=subject, recipients=[user.email])
-    template_path = f'emails/{template}'
-    msg.html = render_template(template_path, name=user.first_name, items=items)
+    with open(f'emails/{template}', 'r') as file:
+        html_content = file.read()
+        html_content = html_content.replace(f'{{name}}', user.first_name)
+        if items:
+            html_content = html_content.replace(f'{{items}}', items)
+
+    msg.html = html_content
     return msg
