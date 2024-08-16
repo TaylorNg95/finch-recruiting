645fa56817a26f137bb292fa8287aec26435af12
Taylor Ng <taylormng95@gmail.com>
2024-08-16 09:24:32 -0400

    styles high priority on recruit card

--- client/src/components/cards/RecruitCard.jsx
+++ client/src/components/cards/RecruitCard.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
 
 // Material UI
-import { Grid, Paper, Typography } from '@mui/material'
-
-// RecruitCard component
-
+import { Grid, Paper, Typography, Button } from '@mui/material'
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarIcon from '@mui/icons-material/Star';
 
 function RecruitCard({recruit}) {
+  const {editRecruit} = useContext(UserContext)
 
   return (
     <Grid item xs={12} sm={4} md={3}>
       <Paper elevation={3} style={{ padding: '5%', backgroundColor: '#D3D3D3' }}>
-        <Typography component='h3' variant='h6' sx={{mb: '2%'}}>{`${recruit.first_name} ${recruit.last_name}`}</Typography>
-        <Link to={`/recruits/${recruit.id}`}>View Profile</Link>
-      </Paper>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+        <Typography component='h3' variant='h6' sx={{ mb: '2%', fontWeight: 'bold' }}>
+          {`${recruit.first_name} ${recruit.last_name}`}
+        </Typography>
+        <Button onClick={() => editRecruit({high_priority: !recruit.high_priority}, recruit.id)}>{recruit.high_priority ? <StarIcon sx={{color: '#000000'}}/> : <StarBorderIcon sx={{color: '#000000'}}/>}</Button>
+      </div>
+      <Link to={`/recruits/${recruit.id}`} style={{textDecoration: 'none', color: '#000000'}}>View Profile</Link>
+    </Paper>
     </Grid>
   )
 }
